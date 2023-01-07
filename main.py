7fed9d4cd2f7c64e075b2b52ff60441cf8eb64e9
Tiago Valente <tiagomilheiro13@outlook.pt>
2023-01-07 19:31:08 +0000

    fixed type error on main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.config import *
 def main():
     """ Main function """
     if len(sys.argv) == 4:  # if received resolution, sound on/off as arguments
-        settings = GlobalSettings((int(sys.argv[1]), int(sys.argv[2])), sys.argv[3])
+        settings = GlobalSettings((int(sys.argv[1]), int(sys.argv[2])), bool(sys.argv[3]))
 
     else:  # if not, use default resolution and sound on
         settings = GlobalSettings()
